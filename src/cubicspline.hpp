050e83eea39daab986d3e3fb98ebc0784b032751
FinancialEngineerLab <hyunjin506@naver.com>
2021-07-25 16:44:53 +0900

    3

---
+++ src/cubicspline.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+class CubicSpline
+{
+    public:
+    CubicSpline() {}
+    ~CubicSpline() {}
+    
+    void add(double x,)
+    
+}
---
+++ src/intepolator.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <map>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+class Interpolator
+{
+    public:
+    Interpolator() {}
+    ~Interpolator() {}
+
+    virtual void add(double x, double y) const = 0;
+    virtual double value(double x) const = 0;
+
+    private:
+    map<double, double> DataPoints;
+};
+
+class LinearInterpolator : public Interpolator
+{
+    public:
+    LinearInterpolator() {}
+    ~LinearInterpolator() {}
+    
+    virtual void add(double x, double y);
+    virtual double value(double x);
+
+    private:
+};
